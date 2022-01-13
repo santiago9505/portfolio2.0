b8a718bf6dc2a8ebbdec1d430347a541c669cd7e
Santigeek <santigeek@hotmail.com>
2022-01-12 20:57:43 -0500

    adding changes to the new projects section

--- src/components/Experience/Project.js
+++ src/components/Experience/Project.js
@@ -1,33 +1,14 @@
 import React from "react";
-import "../../assets/styles/Experience.css";
 
-class Project extends React.Component {
-  render() {
-    return (
-      <a href={this.props.link} className="project__container">
-        <div className="project__flip--container">
-          <div className="project__flip--front">
-            <img
-              className="project__img"
-              src={this.props.img}
-              alt="imagen-proyecto"
-            />
-            <div className="project__paragraph--container">
-              <h3 className="project__title">{this.props.project}</h3>
-              <article>
-                <h4 className="project__stack--title">Stack</h4>
-                <p className="project__stack">{this.props.stack}</p>
-              </article>
-            </div>
-          </div>
-          <div className="project__flip--back">
-            <h1>{this.props.project}</h1>
-            <p>{this.props.description}</p>
-          </div>
-        </div>
-      </a>
-    );
-  }
-}
+import "../../assets/styles/Project.css";
+
+const Project = ({ source }) => {
+  return (
+    <div className="project__container">
+      <img className="project__img" src={source} alt={source} />
+      <div className="project__line"></div>
+    </div>
+  );
+};
 
 export default Project;
